527aa141d07f315398fbe82fb9adb6d997a76ab8
Udara Neminda Chandrawansa <udaraneminda@gmail.com>
2024-10-05 20:09:52 +0530

    upgrade cars through dialog

--- CarShop.java
+++ CarShop.java
@@ -1,80 +1,139 @@
 import java.util.ArrayList;
-import java.util.Collections;  // Import the Collections class
-import java.util.Scanner;  // Import the Scanner class
+import java.util.Scanner; // Import the Scanner class
 
-public class CarShop{
+public class CarShop {
 
     public static ArrayList<Car> carList = new ArrayList<Car>(); // ArrayList objact to store all cars
 
-    public static void initiateDialog(){
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+    public static void initiateDialog() {
+        try (Scanner myObj = new Scanner(System.in)) {
+            // create seperator lines
+            SepLine mainSep = new SepLine("=", 50);
+            SepLine subSep = new SepLine("-", 50);
 
-        // create seperator lines
-        SepLine mainSep = new SepLine("=", 50);
-        SepLine subSep = new SepLine("-", 50);
+            mainSep.drawLine();
+            System.out.println("> UNC Car Shop");
+            subSep.drawLine();
 
-        mainSep.drawLine();
-        System.out.println("> Hello! This is UNC Car Shop!");
-        subSep.drawLine();
+            System.out.print(
+                    "> What u wanna do fool?\n (1: Create Car | 2: Start Drag Race | 3: Upgrade Cars | 4: Exit) ");
+            int taskNo = myObj.nextInt(); // Read task no
+            switch (taskNo) {
+                case 1:
+                    // create car (Name, Manu, Acc, TS, Handl, Enl, Trl, Brl)
+                    System.out.print("> Name of your car: ");
+                    myObj.nextLine(); // Consume leftover newline
+                    String carName = myObj.nextLine(); // Read car name
 
-        System.out.print("> What u wanna do fool?\n (1: Create Car | 2: Start Drag Race | 3: Upgrade Cars | 4: Exit) ");
-        int taskNo = myObj.nextInt();  // Read task no
-        switch(taskNo){
-            case 1:
-                // create car (Name, Manu, Acc, TS, Handl, Enl, Trl, Brl)
-                System.out.print("> Name of your car: ");
-                myObj.nextLine();  // Consume leftover newline
-                String carName = myObj.nextLine();  // Read car name
-                
-                System.out.print("> Car Manufacturer: ");
-                String carManufacturer = myObj.nextLine();  // Read car manufacturer
-                
-                System.out.print("> Acceleration: ");
-                float carAcceleration = myObj.nextFloat();  // Read car acceleration
-                
-                System.out.print("> Top Speed: ");
-                int carTopSpeed = myObj.nextInt();  // Read car top speed
-                
-                System.out.print("> Handling: ");
-                float carHandling = myObj.nextFloat();  // Read car handling
-                
-                // create car
-                Car c1 = new Car(carName, carManufacturer, carAcceleration, carTopSpeed, carHandling, 0, 0, 0);
-                c1.displayInfo();
-                carList.add(c1);
+                    System.out.print("> Car Manufacturer: ");
+                    String carManufacturer = myObj.nextLine(); // Read car manufacturer
 
-                initiateDialog();
-                break;
-            case 2:
-                // drag race
-                if(carList.size() == 2){
-                    initiateDragRace(carList.get(0), carList.get(1));
-                }else{
-                    System.out.println("> Invalid number of participants!");
-                }
-                initiateDialog();
-                break;
-            case 3:
-                // upgrade
-                System.out.println("> lets upgrade!");
-                break;
-            case 4:
-                // exit
-                System.out.println("> GoodBye!");
-                break;
-            default:
-                System.out.println("> Invalid input!");
-                initiateDialog();
-                break;
+                    System.out.print("> Acceleration: ");
+                    float carAcceleration = myObj.nextFloat(); // Read car acceleration
+
+                    System.out.print("> Top Speed: ");
+                    int carTopSpeed = myObj.nextInt(); // Read car top speed
+
+                    System.out.print("> Handling: ");
+                    float carHandling = myObj.nextFloat(); // Read car handling
+
+                    // create car
+                    Car c1 = new Car(carName, carManufacturer, carAcceleration, carTopSpeed, carHandling, 0, 0, 0);
+                    c1.displayInfo();
+                    carList.add(c1);
+
+                    initiateDialog();
+                    break;
+                case 2:
+                    // drag race
+                    if (carList.size() == 2) {
+                        initiateDragRace(carList.get(0), carList.get(1));
+                    } else {
+                        System.out.println("> Invalid number of participants!");
+                    }
+                    initiateDialog();
+                    break;
+                case 3:
+                    // upgrade
+                    System.out.println("> Let's upgrade!");
+                    // list down all car names
+                    for (Car i : carList) {
+                        System.out.println("> You have: " + i.carName + ": " + i.hashCode());
+                    }
+                    // select a car to upgrade
+                    System.out.print("> To select a car to upgrade, input it's number here: ");
+                    int carToUp = myObj.nextInt();
+
+                    // validate selected car
+                    for (Car i : carList) {
+                        boolean validCar = false;
+                        if (i.hashCode() == carToUp) {
+                            validCar = true; // a valid car is selected
+                            // provide upgrade options
+                            System.out.print(
+                                    "> Upgrade Engine: `UE` | Upgrade Transmission: `UT` | Upgrade Brakes: `UB` ");
+                            myObj.nextLine();
+                            String upgradeMethod = myObj.nextLine();
+                            switch (upgradeMethod) {
+                                case "UE":
+                                    // engine upgrade needed
+                                    System.out.print("> Engine Upgrade Level: ");
+                                    int engineUpLvl = myObj.nextInt();
+                                    i.upgradeEngine(engineUpLvl);
+                                    break;
+                                case "UT":
+                                    // transmission upgrade needed
+                                    System.out.print("> Transmission Upgrade Level: ");
+                                    int transmissionUpLvl = myObj.nextInt();
+                                    i.upgradeTransmission(transmissionUpLvl);
+                                    break;
+                                case "UB":
+                                    // brakes upgrade needed
+                                    System.out.print("> Brake Upgrade Level: ");
+                                    int brakeUpLvl = myObj.nextInt();
+                                    i.upgradeBrakes(brakeUpLvl);
+                                    break;
+                                default:
+                                    System.out.println("> Invalid upgrade command used!");
+                                    break;
+                            }
+                            System.out.println();
+                            i.displayInfo();
+                        }
+                        if (!validCar) {
+                            System.out.println("You have not selected a valid car!");
+                        }
+                    }
+                    initiateDialog();
+                    break;
+                case 4:
+                    // exit
+                    System.out.println("> GoodBye!");
+                    break;
+                default:
+                    System.out.println("> Invalid input!");
+                    initiateDialog();
+                    break;
+            }
         }
     }
 
     public static void main(String[] args) {
         // create seperator lines
         SepLine mainSep = new SepLine("=", 50);
-        SepLine subSep = new SepLine("-", 50);
+        SepLine subSep = new SepLine("|", 50);
 
         initiateDialog();
+
+        System.out.println();
+        subSep.drawLine();
+        System.out.println();
+
+        for(Car c : carList){
+            c.displayInfo();
+        }
+
+        /*
 
         // create a stock car & display info
         Car civic = new Car("Civic", "Honda", 0.3f, 110, 0.3f, 0, 0, 0);
@@ -111,13 +170,14 @@ public class CarShop{
         // display info (gallardo)
         gallardo.displayInfo();
         System.out.println();
-        
+
         initiateDragRace(civic, gallardo);
-        
+         */
+
         mainSep.drawLine();
     }
 
-    public static void initiateDragRace(Car car1, Car car2){
+    public static void initiateDragRace(Car car1, Car car2) {
         // start race
         System.out.println("# Drag Race Started!");
 
@@ -125,7 +185,7 @@ public class CarShop{
         double firstCarAvg = car1.carAcceleration * car1.carTopSpeed * car1.carHandling;
         double secondCarAvg = car2.carAcceleration * car2.carTopSpeed * car2.carHandling;
 
-        if(firstCarAvg > secondCarAvg){
+        if (firstCarAvg > secondCarAvg) {
             System.out.println("> " + car1.carName + " has won! Race points: " + firstCarAvg);
         } else {
             System.out.println("> " + car2.carName + " has won! Race points: " + secondCarAvg);
@@ -133,7 +193,7 @@ public class CarShop{
     }
 }
 
-class Car{
+class Car {
     public String carName;
     public String carManufacturer;
     public float carAcceleration; // 0.0 to 5.0
@@ -143,7 +203,8 @@ class Car{
     public int carTransmissionLvl; // affects 'carTopSpeed'
     public int carBrakeLvl; // affects 'carHandling'
 
-    public Car(String carName, String carManufacturer, float carAcceleration, int carTopSpeed, float carHandling, int carEngineLvl, int carTransmissionLvl, int carBrakeLvl){
+    public Car(String carName, String carManufacturer, float carAcceleration, int carTopSpeed, float carHandling,
+            int carEngineLvl, int carTransmissionLvl, int carBrakeLvl) {
         this.carName = carName;
         this.carManufacturer = carManufacturer;
         this.carAcceleration = carAcceleration;
@@ -154,27 +215,28 @@ class Car{
         this.carBrakeLvl = carBrakeLvl;
     }
 
-    public void upgradeEngine(int enginePoints){
+    public void upgradeEngine(int enginePoints) {
         this.carEngineLvl += enginePoints;
         pointsCounter(1);
     }
 
-    public void upgradeTransmission(int transmissionPoints){
+    public void upgradeTransmission(int transmissionPoints) {
         this.carTransmissionLvl += transmissionPoints;
         pointsCounter(2);
     }
 
-    public void upgradeBrakes(int brakePoints){
+    public void upgradeBrakes(int brakePoints) {
         this.carBrakeLvl += brakePoints;
         pointsCounter(3);
     }
 
-    public void pointsCounter(int upgradePart){ // method to count all engine, transmission, brake points and calculate car performance (Acceleration, Top Speed, Handling)
+    public void pointsCounter(int upgradePart) { // method to count all engine, transmission, brake points and calculate
+                                                 // car performance (Acceleration, Top Speed, Handling)
         // engine -> acceleration | transmission -> top speed | brakes -> handling
-        switch(upgradePart){
+        switch (upgradePart) {
             case 1:
                 // upgrade engine -> accelaration
-                switch(this.carEngineLvl){
+                switch (this.carEngineLvl) {
                     case 1:
                         // increase acceleration by 1.0
                         this.carAcceleration += 1.0;
@@ -192,13 +254,13 @@ class Car{
                         break;
                 }
                 // trim to 5.0 if goes beyond 5.0
-                if(this.carAcceleration > 5.0){
+                if (this.carAcceleration > 5.0) {
                     this.carAcceleration = 5.0f;
                 }
                 break;
             case 2:
                 // upgrade transmission -> top speed
-                switch(this.carTransmissionLvl){
+                switch (this.carTransmissionLvl) {
                     case 1:
                         // increase top speed by 30
                         this.carTopSpeed += 30;
@@ -218,7 +280,7 @@ class Car{
                 break;
             default:
                 // upgrade brakes -> handling
-                switch(this.carBrakeLvl){
+                switch (this.carBrakeLvl) {
                     case 1:
                         // increase handling by 1.0
                         this.carHandling += 1.0;
@@ -236,32 +298,34 @@ class Car{
                         break;
                 }
                 // trim to 5.0 if goes beyond 5.0
-                if(this.carHandling > 5.0){
+                if (this.carHandling > 5.0) {
                     this.carHandling = 5.0f;
                 }
                 break;
         }
     }
 
-    public void displayInfo(){
-        System.out.println("> " + this.carManufacturer + " " + this.carName + " with level " + this.carAcceleration + " acceleration, " + this.carTopSpeed + "mph top speed, level " + this.carHandling + " handling.");
-        System.out.println("> Engine Level: " + this.carEngineLvl + " | Transmission Level: " + this.carTransmissionLvl + " | Brakes Level: " + this.carBrakeLvl);
+    public void displayInfo() {
+        System.out.println("> " + this.carManufacturer + " " + this.carName + " with level " + this.carAcceleration
+                + " acceleration, " + this.carTopSpeed + "mph top speed, level " + this.carHandling + " handling.");
+        System.out.println("> Engine Level: " + this.carEngineLvl + " | Transmission Level: " + this.carTransmissionLvl
+                + " | Brakes Level: " + this.carBrakeLvl);
     }
 }
 
-class SepLine{
-	private String lineType;
-	private int lineLength;
+class SepLine {
+    private String lineType;
+    private int lineLength;
 
-	public SepLine(String lineType, int lineLength){
-		this.lineType = lineType;
-		this.lineLength = lineLength;
-	}
+    public SepLine(String lineType, int lineLength) {
+        this.lineType = lineType;
+        this.lineLength = lineLength;
+    }
 
-	public void drawLine(){
+    public void drawLine() {
         for (int i = 0; i < lineLength; i++) {
             System.out.print(lineType);
         }
         System.out.print("\n");
-	}
+    }
 }
